68fd655dfc85bac1abb4f70b9706a03e9270b054
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-03-13 12:32:22 +0000

    edited views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from . models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def index(request):
-    context = "<h1>Hello</h1>"
-    return render(request, 'blog/index.html', {'context':context})
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/index.html', {'post': post})
