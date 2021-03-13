0dfb66b72fba3bcec374fd82cc05e2648dd50e46
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-03-13 10:10:48 +0000

    addes static files

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = "<h1>Hello</h1>"
+    return render(request, 'blog/index.html', {'context':context})
