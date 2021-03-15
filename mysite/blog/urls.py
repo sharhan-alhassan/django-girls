e42e665b8fdc4685d8a60ecedd9af19adc0319a3
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-03-15 01:10:43 +0000

    massive edit to all files

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<int:pk>/', views.detail, name='detail'),
+    path('detail/add/', views.add, name='add'),
+    path('detail/<int:pk>/edit/', views.edit, name='edit'),
+    path('detail/<int:pk>/delete/', views.delete, name='delete')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Post
 from django.utils import timezone
+
+from . forms import PostForm
 
 from django.shortcuts import get_object_or_404
 # Create your views here.
@@ -12,3 +14,38 @@ def index(request):
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/detail.html', {'post': post})
+
+def add(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('/', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/add.html', {'form': form})
+
+def edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('/', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/edit.html', {'form': form})
+
+def delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('/', pk=post.pk)
+
+    return render(request, 'blog/delete.html', {'post': post})
